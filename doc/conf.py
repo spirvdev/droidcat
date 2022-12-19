d44966afe7d4977b3fdf8c9af3f916dfca811e94
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-12-19 13:16:17 -0300

    (0.0.1): Main documentation files added

---
+++ doc/conf.py
@@ -0,0 +1,11 @@
+project = 'Droidtool SRE'
+copyright = '2022, Gabriel Correia'
+author = 'Gabriel Correia'
+version = '0.0.1'
+
+extensions = []
+
+templates_path = ['sphinx-templates']
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['sphinx-static']
---
+++ doc/index.rst
@@ -0,0 +1,15 @@
+Welcome to Droidtool SRE's documentation!
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
