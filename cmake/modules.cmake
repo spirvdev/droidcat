f9720e1a513d1bf75caf36e9966c7b99413ec3d0
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-12-19 23:42:15 -0300

    (0.0.1): CMake modules support added

---
+++ cmake/modules.cmake
@@ -0,0 +1,5 @@
+
+function(droidcat_add_module MODULE_OBJECT MODULE_ID MODULE_VERSION)
+    set_target_properties(${MODULE_OBJECT} PROPERTIES PREFIX ${MODULE_ID}-)
+    set_target_properties(${MODULE_OBJECT} PROPERTIES SUFFIX -${MODULE_VERSION}.dm)
+endfunction()
