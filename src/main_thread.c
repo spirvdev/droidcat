20f58c84e4a0457b675602f60c712e1dbd42b8b4
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-12-19 22:51:42 -0300

    (0.0.1): backend and droidcat context added

--- src/main_thread.c
+++ src/main_thread.c
@@ -1,4 +1,21 @@
-int main()
+
+#include <stdint.h>
+
+typedef int32_t i32;
+
+typedef struct user_options {
+} user_options_t;
+
+typedef struct backend_ctx {
+} backend_ctx_t;
+
+typedef struct droidcat_ctx {
+    user_options_t* user_cmd_options;
+    backend_ctx_t* backend_manager;
+
+} droidcat_ctx_t;
+
+i32 main()
 {
 
 }
