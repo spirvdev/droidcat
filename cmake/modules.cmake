f42a025b6758aeef12921bb4c444dd996e277dd4
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-12-22 20:23:18 -0300

    (0.0.1): Specifyng a target when using include_directories rules

--- cmake/modules.cmake
+++ cmake/modules.cmake
@@ -2,4 +2,6 @@
 function(droidcat_add_module MODULE_OBJECT MODULE_ID MODULE_VERSION)
     set_target_properties(${MODULE_OBJECT} PROPERTIES PREFIX ${MODULE_ID}-)
     set_target_properties(${MODULE_OBJECT} PROPERTIES SUFFIX -${MODULE_VERSION}.dm)
+    target_include_directories(${MODULE_OBJECT} PRIVATE ${CMAKE_SOURCE_DIR}/include)
+
 endfunction()
